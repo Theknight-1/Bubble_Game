885a12cb8d116b2e36c5401fc06a64ac2c73b42b
Himanshu <himanshusaxena5000@gmail.com>
2024-01-13 12:48:55 +0530

    Completed

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function makeBubble(){
     const box = document.querySelector('.p-bottom');
-var clutter = "";
-for (let index = 0; index < 114; index++) {
-   clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`
-}
-box.innerHTML = clutter
+    var clutter = "";
+    for (let index = 0; index < 90; index++) {
+        clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`
+    }
+    box.innerHTML = clutter
 }
 makeBubble();
-
 
 var timer = 60;
 function runTimer(){
@@ -17,14 +16,45 @@ function runTimer(){
             document.querySelector('.timer').textContent = timer
         }
         else{
+            const score = document.querySelector('#score').textContent;
+            document.querySelector('.p-bottom').innerHTML = `<div class="reset">
+                <h2>Your Score ${score}</h2>
+                <h3>Game Over</h3>
+                <button class="playAgainBtn">Play Again</button>
+            </div>`;
+            document.querySelector('.playAgainBtn').addEventListener('click',playAgain);
             clearInterval(moveTimer)
         }
     },1000)
 }
 runTimer();
 
+
+var randomNumber = 0;
 function getNewHit (){
-    const randomNumber = Math.floor(Math.random()*10);
+    randomNumber = Math.floor(Math.random()*10);
     document.querySelector("#hit-value").textContent = randomNumber;
 }
-getNewHit() 
+getNewHit();
+
+document.querySelector('.p-bottom').addEventListener('click',function(dets){
+    var value = Number(dets.target.textContent);
+    if(value === randomNumber){
+        increaseScore()
+    }
+})
+
+var score = 0;
+function increaseScore(){
+    score += 10;
+    document.querySelector('#score').textContent = score;
+    makeBubble();
+    getNewHit();
+}
+
+
+//restart the game
+function playAgain(){
+    console.log("click hua");
+    window.location.reload();
+}
